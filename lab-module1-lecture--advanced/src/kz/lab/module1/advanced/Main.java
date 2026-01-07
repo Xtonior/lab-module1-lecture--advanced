a5d668c657c6406d009d5ba5f0174e935ab6b602
Xtonior <83264344+Xtonior@users.noreply.github.com>
2026-01-07 13:03:01 +0500

    tweak

--- lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Main.java
+++ lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         // taskImmutable();
         // taskRecord();
-        taskLambda();
+        // taskLambda();
     }
 
     private static void taskImmutable() {
